cd26d14e4785dd4728d702d486c549c34efc3fcb
André Pereira <andrewpereira.megabyte@gmail.com>
2022-12-03 20:10:59 -0300

    Add files via upload

---
+++ andrew.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+clear
+echo "\n"
+echo "      ___           ___           ___           ___           ___           ___     "
+echo "     /\  \         /\__\         /\  \         /\  \         /\  \         /\__\    "
+echo "    /::\  \       /::|  |       /::\  \       /::\  \       /::\  \       /:/ _/_   "
+echo "   /:/\:\  \     /:|:|  |      /:/\:\  \     /:/\:\  \     /:/\:\  \     /:/ /\__\  "
+echo "  /::\~\:\  \   /:/|:|  |__   /:/  \:\__\   /::\~\:\  \   /::\~\:\  \   /:/ /:/ _/_ "
+echo " /:/\:\ \:\__\ /:/ |:| /\__\ /:/__/ \:|__| /:/\:\ \:\__\ /:/\:\ \:\__\ /:/_/:/ /\__\\"
+echo " \/__\:\/:/  / \/__|:|/:/  / \:\  \ /:/  / \/_|::\/:/  / \:\~\:\ \/__/ \:\/:/ /:/  /"
+echo "      \::/  /      |:/:/  /   \:\  /:/  /     |:|::/  /   \:\ \:\__\    \::/_/:/  / "
+echo "      /:/  /       |::/  /     \:\/:/  /      |:|\/__/     \:\ \/__/     \:\/:/  /  "
+echo "     /:/  /        /:/  /       \::/__/       |:|  |        \:\__\        \::/  /   "
+echo "     \/__/         \/__/         ~~            \|__|         \/__/         \/__/    "
+echo "\n------------------------------------------------------------------------------------\n"
+
+if [ $1 = "-i" ] && [ $2 = "schonheit" ]
+then
+    echo "\n🌐️ Downloading Schonheit..."
+    wget "https://redwars22.github.io/Schonheit/schonheit.min.css"
+    echo "✅️ Schonheit has been downloaded successfully!!!"
+    nautilus .
+    exit 1
+fi
+
+if [ $1 = "-i" ] && [ $2 = "titanium" ]
+then
+    mkdir titanium && cd titanium
+    echo "\n🌐️ Downloading AndrewToolchain - Titanium's Interpreter..."
+    wget "https://github.com/Redwars22/AndrewToolchain/releases/download/v1.4.0-release-candidate/titanium_v1.4.0_rc.zip"
+    echo "\n📦️ Unzipping AndrewToolchain..."
+    unzip "titanium_v1.4.0_rc.zip"
+    echo "\n⚙️  Compilling Titanium..."
+    tsc titanium.ts
+    nautilus .
+    exit 1
+fi
+
+if [ $1 = "-h" ] && [ $2 = "pt" ]
+then
+    clear
+    cat help-pt.txt
+    echo "\n"
+    exit 1
+fi
+
+if [ $1 = "-h" ]
+then
+    clear
+    cat help.txt
+    echo "\n"
+    exit 1
+fi
+
+echo "\n🛑️ Invalid arguments! Please use -h to read AndrewSH's help file"
