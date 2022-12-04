641fd2224335478852caf1899f1a5e341bdbdfbd
~Andrew <andrewpereira.megabyte@gmail.com>
2022-12-04 08:56:52 -0300

    update Andrew with all my projects

--- andrew.sh
+++ andrew.sh
@@ -41,10 +41,76 @@ if [ $1 = "-i" ] && [ $2 = "gustavoevil-game" ]
 then
     echo "\n🌐️ Downloading Gustavo Evil: O Retorno do Norton..."
     wget "https://github.com/Redwars22/GustavoEvil/releases/download/v0.0.7-demo/GustavoEvil-1.0-pc.zip"
+    echo "\n📦️ Unzipping Gustavo Evil: O Retorno do Norton..."
     unzip "GustavoEvil-1.0-pc.zip"
     cd "GustavoEvil-1.0-pc"
     echo "\n🎮️ Opening Gustavo Evil: O Retorno do Norton..."
     sh GustavoEvil.sh
+    exit 1
+fi
+
+if [ $1 = "-i" ] && [ $2 = "evil" ]
+then
+    echo "\n🌐️ Downloading EvilCSS..."
+    mkdir "evil-bin" && cd "evil-bin"
+    wget "https://github.com/Redwars22/Evil/releases/download/v0.0.5/Evil.zip"
+    echo "\n📦️ Unzipping EvilCSS..."
+    unzip Evil.zip
+    echo "\n⚙️  Setting EvilCSS up and opening its interpreter..."
+    ruby evil.rb
+    code example.evil
+    exit 1
+fi
+
+if [ $1 = "-i" ] && [ $2 = "gustavojs" ]
+then
+    echo "\n🌐️ Downloading GustavoJS with NPM..."
+    npm install gustavojs
+    cd node_modules/gustavojs
+    echo "\n🎮️ Opening your GustavoJS project in Visual Studio Code..."
+    code .
+    exit 1
+fi
+
+if [ $1 = "-i" ] && [ $2 = "abbie-cli" ]
+then
+    echo "\n🌐️ Downloading AbbieCLI..."
+    wget "https://github.com/Redwars22/abbie/releases/download/experimental2/abbie.jar"
+    echo "\nOpening AbbieCLI with Java (⚠️ Please make sure you have it installed already)..."
+    java -jar "abbie.jar"
+    exit 1
+fi
+
+if [ $1 = "-i" ] && [ $2 = "arctic" ]
+then
+    echo "\n🌐️ Downloading Arctic..."
+    mkdir arctic-bin && cd arctic-bin
+    wget "https://github.com/Redwars22/Arctic/releases/download/v0.4.0-alpha/arctic_v0.4.0_alpha.zip"
+    echo "\n📦️ Unzipping Arctic..."
+    unzip "arctic_v0.4.0_alpha.zip"
+    echo "\nOpening Arctic Interpreter..."
+    ruby arctic.rb
+    code example.ice
+    exit 1
+fi
+
+if [ $1 = "-u" ]
+then
+    clear
+    echo "\n"
+    echo "      ___           ___           ___           ___           ___           ___     "
+    echo "     /\  \         /\__\         /\  \         /\  \         /\  \         /\__\    "
+    echo "    /::\  \       /::|  |       /::\  \       /::\  \       /::\  \       /:/ _/_   "
+    echo "   /:/\:\  \     /:|:|  |      /:/\:\  \     /:/\:\  \     /:/\:\  \     /:/ /\__\  "
+    echo "  /::\~\:\  \   /:/|:|  |__   /:/  \:\__\   /::\~\:\  \   /::\~\:\  \   /:/ /:/ _/_ "
+    echo " /:/\:\ \:\__\ /:/ |:| /\__\ /:/__/ \:|__| /:/\:\ \:\__\ /:/\:\ \:\__\ /:/_/:/ /\__\\"
+    echo " \/__\:\/:/  / \/__|:|/:/  / \:\  \ /:/  / \/_|::\/:/  / \:\~\:\ \/__/ \:\/:/ /:/  /"
+    echo "      \::/  /      |:/:/  /   \:\  /:/  /     |:|::/  /   \:\ \:\__\    \::/_/:/  / "
+    echo "      /:/  /       |::/  /     \:\/:/  /      |:|\/__/     \:\ \/__/     \:\/:/  /  "
+    echo "     /:/  /        /:/  /       \::/__/       |:|  |        \:\__\        \::/  /   "
+    echo "     \/__/         \/__/         ~~            \|__|         \/__/         \/__/    "
+    echo "\n------------------------------------------------------------------------------------\n"
+    echo "Until I develop a system for updating Andrew, you can just clone its repo (https://github.com/Redwars22/Andrew) and use **git pull** to download the latest changes."
     exit 1
 fi
 
